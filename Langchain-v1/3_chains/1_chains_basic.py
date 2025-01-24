c923322ee85049b320e891b248ddf6e9edc7f6fe
Deepak <ideepakrabindra@gmail.com>
2025-01-24 21:49:58 +0530

    feat: chains basic

---
+++ Langchain-v1/3_chains/1_chains_basic.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+llm = ChatOpenAI(model_name="gpt-4o", temperature=0.0)
+
+# Define prompt template with input variables (No need for separate Runnable chains)
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a fact expert who knows facts about {animal}"),
+        ("human", "Tell me {fact_count} facts."),
+    ]
+)
+
+# Create a chain with the prompt template and output parser
+chain = prompt_template | llm | StrOutputParser()
+
+# Run the chain with the input variables
+output = chain.invoke({"animal": "cat", "fact_count": 3})
+
+print(output)
